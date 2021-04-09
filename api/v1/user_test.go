d707f02a92fd1379c6d55250f966cf3012b76e37
akez <1791189210@qq.com>
2021-04-09 11:37:25 +0800

    add register test

--- api/v1/user_test.go
+++ api/v1/user_test.go
@@ -17,31 +17,10 @@ func TestLogin(t *testing.T) {
 	login["username"] = "zwz"
 	login["password"] = "123456"
 
-	bytesData, err := json.Marshal(login)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	reader := bytes.NewReader(bytesData)
 	reqType := "POST"
-	url := "http://localhost:8080/user/login"
+	url := "http://localhost:8000/user/login"
 
-	request, err := http.NewRequest(reqType, url, reader)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
-
-	client := http.Client{}
-
-	resp, err := client.Do(request)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
+	resp := RequestJsonHelper(reqType, url, login)
 
 	respBytes, err := ioutil.ReadAll(resp.Body)
 
@@ -51,7 +30,6 @@ func TestLogin(t *testing.T) {
 	}
 
 	str := (*string)(unsafe.Pointer(&respBytes))
-
 	got := *str
 	want := `{"code":0,"data":{"username":"zwz","password":"123456"},"msg":"Login Success"}`
 
@@ -61,4 +39,61 @@ func TestLogin(t *testing.T) {
 }
 
 func TestRegister(t *testing.T) {
+	register := make(map[string]interface{})
+
+	register["username"] = "oreo"
+	register["password"] = "123456"
+
+	reqType := "POST"
+	url := "http://localhost:8000/user/register"
+
+	_ = RequestJsonHelper(reqType, url, register)
+
+	url = "http://localhost:8000/user/login"
+
+	resp := RequestJsonHelper(reqType, url, register)
+
+	respBytes, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	str := (*string)(unsafe.Pointer(&respBytes))
+	got := *str
+	want := `{"code":0,"data":{"username":"oreo","password":"123456"},"msg":"Login Success"}`
+
+	if got != want {
+		t.Errorf("got '%q' want '%q'", got, want)
+	}
+
+}
+
+func RequestJsonHelper(reqType string, url string, jsonMap map[string]interface{}) *http.Response {
+	bytesData, err := json.Marshal(jsonMap)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+
+	reader := bytes.NewReader(bytesData)
+
+	request, err := http.NewRequest(reqType, url, reader)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+
+	request.Header.Set("Content-Type", "application/json;charset=UTF-8")
+
+	client := http.Client{}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+
+	return resp
 }
