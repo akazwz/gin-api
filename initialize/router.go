c91009a01c7511fd1edc33fcc87e86ee2ace364a
akez <1791189210@qq.com>
2021-04-17 16:30:23 +0800

    418

--- initialize/router.go
+++ initialize/router.go
@@ -5,11 +5,18 @@ import (
 	"github.com/akaedison/go-gin-demo/routers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Routers() *gin.Engine {
 	var router = gin.Default()
 	router.Use(cors.Default())
+
+	router.Any("418", func(c *gin.Context) {
+		c.JSON(http.StatusTeapot, gin.H{
+			"message": "I'm a teapot",
+		})
+	})
 
 	publicRouterV1 := router.Group("v1")
 	{
