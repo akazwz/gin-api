bebae8ab5afb6e79cebf2131190d6bf33cb0c141
akazwz <akazwz@pm.me>
2021-10-21 11:38:59 +0800

    update url

--- api/api.go
+++ api/api.go
@@ -9,14 +9,14 @@ import (
 // api list
 func GetApiList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"teapot":                               "https://api.akazwz.com/teapot",
-		"token by username and password":       "https://api.akazwz.com/v1/token/username-pwd",
-		"token by phone and password":          "https://api.akazwz.com/v1/token/phone-pwd",
-		"token by phone and verification code": "https://api.akazwz.com/v1/token/phone-code",
-		"users":                                "https://api.akazwz.com/v1/users",
-		"users_authority":                      "https://api.akazwz.com/v1/users/authority",
-		"users_password":                       "https://api.akazwz.com/v1/users/password",
-		"books":                                "https://api.akazwz.com/v1/books",
-		"file":                                 "https://api.akazwz.com/v1/file",
+		"teapot":                               "https://api.hellozwz.com/teapot",
+		"token by username and password":       "https://api.hellozwz.com/v1/token/username-pwd",
+		"token by phone and password":          "https://api.hellozwz.com/v1/token/phone-pwd",
+		"token by phone and verification code": "https://api.hellozwz.com/v1/token/phone-code",
+		"users":                                "https://api.hellozwz.com/v1/users",
+		"users_authority":                      "https://api.hellozwz.com/v1/users/authority",
+		"users_password":                       "https://api.hellozwz.com/v1/users/password",
+		"books":                                "https://api.hellozwz.com/v1/books",
+		"file":                                 "https://api.hellozwz.com/v1/file",
 	})
 }
