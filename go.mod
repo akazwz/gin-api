16849091af18ec84fba75c1d761722b778e5b81c
akazwz <akazwz@pm.me>
2022-08-03 17:03:14 +0800

    add mysql

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/joho/godotenv v1.4.0
 	github.com/satori/go.uuid v1.2.0
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d
+	gorm.io/driver/mysql v1.3.5
 	gorm.io/driver/sqlite v1.3.6
 	gorm.io/gorm v1.23.8
 )
@@ -38,6 +39,7 @@ require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/goccy/go-json v0.9.10 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- initialize/gorm.go
+++ initialize/gorm.go
@@ -1,18 +1,46 @@
 package initialize
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"log"
+	"os"
 
 	"github.com/akazwz/gin-api/model"
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func InitGorm() *gorm.DB {
+	if gin.Mode() != "release" {
+		return InitSqlite()
+	} else {
+		return InitMysql()
+	}
+}
+
+func InitSqlite() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		log.Println(err.Error())
-		log.Fatalln("初始化 gorm 失败")
+		log.Fatalln("初始化 sqlite 失败")
+	}
+	return db
+}
+
+func InitMysql() *gorm.DB {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_NAME"),
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println(err.Error())
+		log.Fatalln("初始化 mysql 失败")
 	}
 	return db
 }
