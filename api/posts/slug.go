a9cbb8fbd08584b916728470e80c667bde404870
akazwz <akazwz@pm.me>
2022-08-05 16:59:41 +0800

    get /post/pid add redis cache

--- api/posts/slug.go
+++ api/posts/slug.go
@@ -2,6 +2,10 @@ package posts
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/akazwz/gin-api/model"
+	"time"
 
 	"github.com/akazwz/gin-api/api"
 	"github.com/akazwz/gin-api/global"
@@ -11,14 +15,34 @@ import (
 )
 
 func GetPostById(c *gin.Context) {
-	id := c.Param("id")
 	postService := service.PostService{}
+
+	id := c.Param("id")
+
+	cacheKey := fmt.Sprintf("%s-%s", "cache-post", id)
+	// redis 中获取缓存
+	result, err := global.GREDIS.Get(context.TODO(), cacheKey).Result()
+	// redis 中有缓存
+	if err == nil {
+		var post model.Post
+		_ = json.Unmarshal([]byte(result), &post)
+		_ = postService.AddViewed(id)
+		response.Ok(api.CodeCommonSuccess, post, "success", c)
+		return
+	}
+
 	post, err := postService.FindPostByID(id)
 	if err != nil {
 		response.NotFound(api.CodeCommonFailed, "Not Found", c)
 		return
 	}
 	_ = postService.AddViewed(id)
+
+	// post 转为 json字符串
+	bytes, _ := json.Marshal(post)
+	// 存入缓存
+	_ = global.GREDIS.Set(context.TODO(), cacheKey, string(bytes), 1*time.Minute).Err()
+
 	response.Ok(api.CodeCommonSuccess, post, "success", c)
 }
 
